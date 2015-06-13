a6f589634ab85088503c86c3855f0abb7f20f387
omerakyol <omerakyol36@gmail.com>
2015-06-14 00:23:32 +0300

    Add Files
    Adding repository files.

---
+++ MvcFileUpload/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcFileUploader;
+using MvcFileUploader.Models;
+
+namespace MvcFileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UploadFile()
+        {
+            var statuses = new List<ViewDataUploadFileResult>();
+            for (var i = 0; i < Request.Files.Count; i++)
+            {
+                var st = FileSaver.StoreFile(x =>
+                {
+                    x.File = Request.Files[i];
+                    x.DeleteUrl = Url.Action("DeleteFile");
+                    x.StorageDirectory = Server.MapPath("~/Uploads");
+                    x.UrlPrefix = "/Uploads";
+                    x.FileName = Request.Files[i].FileName;
+                    x.ThrowExceptions = true;
+                });
+                statuses.Add(st);
+            }
+         
+            statuses.ForEach(x => x.thumbnailUrl = x.url + "?width=80&height=80");
+            statuses.ForEach(x => x.url = Url.Action("DownloadFile", new { fileUrl = x.url, mimetype = x.type }));
+            var viewresult = Json(new { files = statuses });
+            //for IE8 which does not accept application/json
+            if (Request.Headers["Accept"] != null && !Request.Headers["Accept"].Contains("application/json"))
+                viewresult.ContentType = "text/plain";
+
+            return viewresult;
+        }
+
+        [HttpPost] 
+        public ActionResult DeleteFile(string fileUrl)
+        {
+            var filePath = Server.MapPath("~" + fileUrl);
+
+            if (System.IO.File.Exists(filePath))
+                System.IO.File.Delete(filePath);
+
+            var viewresult = Json(new { error = String.Empty });
+            //for IE8 which does not accept application/json
+            if (Request.Headers["Accept"] != null && !Request.Headers["Accept"].Contains("application/json"))
+                viewresult.ContentType = "text/plain";
+
+            return viewresult; 
+        }
+
+
+        public ActionResult DownloadFile(string fileUrl, string mimetype)
+        {
+            var filePath = Server.MapPath("~" + fileUrl);
+
+            if (System.IO.File.Exists(filePath))
+                return File(filePath, mimetype);
+            else
+            {
+                return new HttpNotFoundResult("File not found");
+            }
+        }
+
+    }
+}
